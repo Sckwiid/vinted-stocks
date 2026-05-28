b8a5694eb8cd44e54c9115798516310592dc3920
Sckwiid <51514405+Sckwiid@users.noreply.github.com>
2026-05-28 21:41:06 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.APP_CONFIG = {
   users: {
     anthony: {
-      passwordHash: ""
+      passwordHash: "8aea4bec795b0392787d076da04b15405f91fa54f71e837ba0acfd151dd2231c"
     },
     julien: {
-      passwordHash: ""
+      passwordHash: "5fde4b1f9e2420001fe7381cacae8c033d3661aec3fe26c9f5818638d9491818"
     }
   }
 };
